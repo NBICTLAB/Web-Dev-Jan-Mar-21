afec44f42a286e8bcd0326cb363ffecaf126eaf0
NBICT LAB <nbict.lab@gmail.com>
2021-02-14 17:38:36 +0600

    JS Fundamentals 1st Class

---
+++ extra/practice.js
@@ -0,0 +1,16 @@
+// This is NBICT LAB's code
+
+/*********************
+let js = 'awesome';
+console.log(js);
+console.log(7-11+19*2);
+**********************/
+
+let firstName = 'Badhon';
+let lastName = 'Verma';
+
+console.log(firstName + ' ' + lastName + ' is an IT instructor and entrepreneur. ' + firstName + ' founded NBICT LAB in 2018.');
+
+let x = null;
+
+console.log(typeof(x));
